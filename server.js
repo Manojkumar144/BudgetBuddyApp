910dca57217c199d19acba10b05b7820ad740b50
Manojkumar144 <manojsantha1442000@gmail.com>
2024-02-13 18:02:38 +0530

    Change the port number

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const morgan = require('morgan');
 const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.set('views', 'views');
 
